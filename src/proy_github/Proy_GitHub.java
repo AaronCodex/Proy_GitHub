304bf8696336f9529ea96d1c814fe659fcafe3f6
Aarón Moreno <aaron.severoochoa@gmail.com>
2019-12-02 09:31:12 +0100

    iniciamos el proyecto

---
+++ src/proy_github/Proy_GitHub.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proy_github;
+
+/**
+ *
+ * @author Paco Moreno
+ */
+public class Proy_GitHub {
+
+    static String Proy_GitHub(String valor1, String valor2) {
+        if (valor1.equals(valor2)) {
+            return "Perfecto. Has tenido buen criterio.";
+        }
+        if (Integer.valueOf(valor1) < Integer.valueOf(valor2)) {
+            return "Has mejorado, o te ha llegado el éxito inesperado.";
+        }
+        return "Te has confiado. Falta realismo.";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("La calificación de " + args[0] + " : ");
+        Proy_GitHub(args[3], args[4]);
+    }
+
+}
